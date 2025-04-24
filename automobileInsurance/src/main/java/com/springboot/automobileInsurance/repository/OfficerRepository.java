69e23635c089a63126615686b029d096f32feb47
unknown <chararamesh1202@gmail.com>
2025-04-24 19:15:59 +0530

    Insurance Officer model,Controller,service,Repository added

---
+++ automobileInsurance/src/main/java/com/springboot/automobileInsurance/repository/OfficerRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.automobileInsurance.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.automobileInsurance.model.Officer;
+
+public interface OfficerRepository extends JpaRepository<Officer, Integer> {
+
+}
